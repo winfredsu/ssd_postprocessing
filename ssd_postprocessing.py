15ece4c6092e46a52bfd37cc750c1ef2b1ebfe4c
winfredsu <winfredsu@outlook.com>
2019-09-26 10:54:43 +0800

    add 1-class nms

--- ssd_postprocessing.py
+++ ssd_postprocessing.py
@@ -7,9 +7,10 @@ import PIL.ImageDraw as ImageDraw
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disable GPU info
 
+##################################################################
 # GENERAL DEFINES
 INFERENCE_GRAPH = 'oiltank_mnetv2_0.5_ssd.pb'
-TEST_IMAGE = './test2.jpg'
+TEST_IMAGE = './test_images/5.jpg'
 IMAGE_SIZE = [270,480] # [H,W]
 INPUT_TENSOR_NAME = 'normalized_input_image_tensor:0'
 
@@ -22,6 +23,12 @@ ASPECT_RATIOS = (1.0, 2.0, 3.0, 1.0/2, 1.0/3)
 BOX_CODER_SCALE = [10.0, 10.0, 5.0, 5.0]  # [y_scale, x_scale, h_scale, w_scale]
 ENDPOINT_CLASS = ['BoxPredictor_'+str(i)+'/ClassPredictor/act_quant/FakeQuantWithMinMaxVars:0' for i in range(NUM_SSD_LAYERS)]
 ENDPOINT_BOX   = ['BoxPredictor_'+str(i)+'/BoxEncodingPredictor/act_quant/FakeQuantWithMinMaxVars:0' for i in range(NUM_SSD_LAYERS)]
+
+# CHANGE THESE PARAMS FOR BETTER DEMONSTRATION
+SCORE_THRESHOLD = -0.1
+MAX_NMS_CANDIDATES = 50
+NMS_THRESHOLD = 0.6
+##################################################################
 
 def create_ssd_anchors(num_layers=6,
                        min_scale=0.2,
@@ -113,18 +120,29 @@ def create_ssd_anchors(num_layers=6,
 
   return box_specs_list
 
-def draw_bbox(image, box):
+def draw_bbox(size, draw, box):
     ymin = box[0]
     xmin = box[1]
     ymax = box[2]
     xmax = box[3]
-    image = PIL.Image.open(TEST_IMAGE)
-    draw = PIL.ImageDraw.Draw(image)
-    im_width, im_height = image.size
+
+    im_width, im_height = size
     (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
                                   ymin * im_height, ymax * im_height)
     draw.line([(left, top), (left, bottom), (right, bottom),
              (right, top), (left, top)], width=4, fill='blue')
+
+def draw_bboxes(boxes):
+    """
+    input image: path to image
+    input boxes: 2d list, [NUM_BOXES, 4]
+    """
+    image = PIL.Image.open(TEST_IMAGE)
+    draw = PIL.ImageDraw.Draw(image)
+
+    for box in boxes:
+        draw_bbox(image.size, draw, box)
+
     image.show()
 
 def decode(encoded_box, anchor_box):
@@ -154,10 +172,10 @@ def decode(encoded_box, anchor_box):
     h = np.exp(th) * ha
     ycenter = ty * ha + ycenter_a
     xcenter = tx * wa + xcenter_a
-    ymin = ycenter - h / 2.
-    xmin = xcenter - w / 2.
-    ymax = ycenter + h / 2.
-    xmax = xcenter + w / 2.   
+    ymin = max(0., ycenter - h / 2.)
+    xmin = max(0., xcenter - w / 2.)
+    ymax = min(1., ycenter + h / 2.)
+    xmax = min(1., xcenter + w / 2.)   
     
     return [ymin,xmin,ymax,xmax]
 
@@ -188,6 +206,98 @@ def get_anchor_box_from_preds(fmap_shape, pred_id, box_spec):
 
     return [ycenter_a, xcenter_a, ha, wa]
 
+def get_anchor_box_from_candidate(id, box_specs, endpoint_shapes):
+    """
+    input: candidate id in range(sum(H*W*ANCHORS))
+    input: box_specs, a list of (scale, ratio) for all ssd layers
+    input: endpoint_shapes, a list of (H, W, ANCHORS) for all ssd layers
+    return: anchor_box [ymin, xmin, ymax, xmax]
+    """
+    threshold = 0
+    for layer_id in range(NUM_SSD_LAYERS):
+        endpoint_shape = endpoint_shapes[layer_id]
+        endpoint_shape_flatten = endpoint_shape[0]*endpoint_shape[1]*endpoint_shape[2] # H*W*ANCHORS
+        threshold += endpoint_shape_flatten
+        if id < threshold:
+            id -= (threshold-endpoint_shape_flatten)
+            break
+
+    # now we have layer_id and the flattened pixel id in that layer
+    # 1. get the feature map shape
+    fmap_shape = endpoint_shapes[layer_id]
+    # 2. get the pred_id in [y,x,boxid]
+    pred_id = np.unravel_index(id, fmap_shape)
+    # 3. get the box_spec of the current layer
+    box_spec = box_specs[layer_id]
+    
+    anchor_box_center = [(pred_id[i]+0.5)/fmap_shape[i] for i in range(2)]
+    ycenter_a = anchor_box_center[0]
+    xcenter_a = anchor_box_center[1]
+
+    scale, ratio = box_spec[pred_id[2]]
+    wa = scale*np.sqrt(ratio)
+    ha = scale/np.sqrt(ratio)
+
+    # handle the non-square input with square anchor boxes
+    # IMAGE_SIZE = [H,W]
+    # the anchor boxes in the training process are square and relative to min(H,W)
+    # we need to re-scale the ha or wa here
+    # so that the follow-up scripts can treat (ha,wa) as ratios relative to (H,W) 
+    if IMAGE_SIZE[0]>IMAGE_SIZE[1]: # H>W
+      ha *= (IMAGE_SIZE[1]/IMAGE_SIZE[0])
+    else: # W>H
+      wa *= (IMAGE_SIZE[0]/IMAGE_SIZE[1])
+
+    return [ycenter_a, xcenter_a, ha, wa]
+
+def calc_iou(box1, box2):
+    """
+    calculate the iou of two boxes
+    Args:
+        box1, box2: [ymin,xmin,ymax,xmax]
+    Return: 
+        iou
+    """
+    # get the coordinates
+    ymin1,xmin1,ymax1,xmax1 = box1
+    ymin2,xmin2,ymax2,xmax2 = box2
+
+    # calc the areas of two boxes
+    area1 = (xmax1-xmin1)*(ymax1-ymin1)
+    area2 = (xmax2-xmin2)*(ymax2-ymin2)
+
+    # get the posible intersection boundries
+    xx1 = max(xmin1, xmin2)
+    yy1 = max(ymin1, ymin2)
+    xx2 = min(xmax1, xmax2)
+    yy2 = min(ymax1, ymax2)
+
+    # calc the intersection area
+    intersection_area = max(0., xx2-xx1) * max(0., yy2-yy1)
+
+    return intersection_area / (area1+area2-intersection_area)+1e-6
+
+def non_maximum_supression(candidates, threshold=NMS_THRESHOLD):
+    """
+    do NMS on the candidates
+    Args:
+        candidates: array of bboxes of a specific class sorted by score in descending order
+        threshold: nms threshold
+    Returns:
+        results: final predictions
+    """
+    for idx,m in enumerate(candidates):
+        for c in candidates[idx+1:]:
+            if calc_iou(m,c) > NMS_THRESHOLD:
+                candidates.remove(c)
+    
+    return candidates
+
+
+# get box specs (scale, aspect_ratio)
+# for SSDs with 6 layers, the number of anchor boxes are [3,6,6,6,6,6]
+box_specs = create_ssd_anchors(num_layers=NUM_SSD_LAYERS, min_scale=MIN_SCALE, max_scale=MAX_SCALE)
+
 # load graph
 f = open(INFERENCE_GRAPH, 'rb')
 gd = tf.GraphDef.FromString(f.read())
@@ -200,36 +310,53 @@ test_image = np.array([np.array(PIL.Image.open(TEST_IMAGE).resize([480,270])).as
 # eval the endpoints
 g = tf.get_default_graph()
 with tf.Session() as sess:
-    pred_class = sess.run(ENDPOINT_CLASS, feed_dict={INPUT_TENSOR_NAME: test_image})
-    pred_box   = sess.run(ENDPOINT_BOX  , feed_dict={INPUT_TENSOR_NAME: test_image})
+    classes = sess.run(ENDPOINT_CLASS, feed_dict={INPUT_TENSOR_NAME: test_image})
+    boxes   = sess.run(ENDPOINT_BOX  , feed_dict={INPUT_TENSOR_NAME: test_image})
 
-# get box specs (scale, aspect_ratio)
-# for SSDs with 6 layers, the number of anchor boxes are [3,6,6,6,6,6]
-box_specs = create_ssd_anchors(num_layers=NUM_SSD_LAYERS, min_scale=MIN_SCALE, max_scale=MAX_SCALE)
+# get the endpoint shapes
+endpoint_shapes = [] 
+for p in classes:
+    endpoint_shapes.append(list(p.shape[1:3])+[int(p.shape[3]/(NUM_CLASSES+1))]) # (H,W,ANCHORS)
 
-## Test
-# below is an example of calculating the bbox corresponding to the max score in a specified SSD output feature map
-# NMS has not been implemented yet
-layer_id = 0
-# squeeze along the N dim
-preds = pred_class[layer_id].squeeze(0)
-boxes = pred_box[layer_id].squeeze(0)
-preds_shape = preds.shape
-# reshape preds to [H, W, ANCHORS, CLASSES+1], boxes to [H, W, ANCHORS, 4]
-preds = preds.reshape([preds_shape[0], preds_shape[1], -1, NUM_CLASSES+1])
-boxes = boxes.reshape([preds_shape[0], preds_shape[1], -1, 4])
-# discard the backgrounds in class predictions (0 for the background)
-preds = preds[:,:,:,1:]
-# find the id of max score [y,x,anchor_id]
-pred_id = np.unravel_index(np.argmax(preds), preds.shape)
-# get anchor box from pred_id
-anchorbox = get_anchor_box_from_preds(preds.shape, pred_id, box_specs[layer_id])
-# get the box encoding
-encoding = boxes[pred_id[0], pred_id[1], pred_id[2],:]
-# decode the box
-result = decode(encoding, anchorbox)
-# draw box
-draw_bbox(TEST_IMAGE, result)
+# reshape the output tensors
+pred_class = np.zeros((0,NUM_CLASSES+1))
+pred_box = np.zeros((0,4))
+for pc,pb in zip(classes, boxes):
+    pred_class = np.concatenate((pred_class, pc.reshape(-1, NUM_CLASSES+1)))
+    pred_box = np.concatenate((pred_box, pb.reshape(-1, 4)))
+# discard the backgrounds
+pred_class = pred_class[:,1:]
+
+#################################################
+# FROM NOW ON WE FOCUS ON THE FIRST CLASS
+pred_class = pred_class[:,0]
+
+# argsort the pred_class (decend)
+pred_class_argsort = pred_class.argsort()[::-1]
+# find the indexes of largest scores that are greater than SCORE_THRESHOLD
+pred_class_candidates = []
+for i in range(MAX_NMS_CANDIDATES):
+    if pred_class[pred_class_argsort[i]] < SCORE_THRESHOLD:
+        break
+    pred_class_candidates.append(pred_class_argsort[i])
+
+# for each candidate, get its encoding and anchor box, and then decode
+decoded_candidates = []
+for c in pred_class_candidates:
+    # get the encoding
+    encoding = pred_box[c,:]
+    # get the anchor box from c
+    anchorbox = get_anchor_box_from_candidate(c, box_specs, endpoint_shapes)
+    # decode the box
+    decoded_candidates.append(decode(encoding, anchorbox))
+
+# do NMS (the decoded_candidates is already sorted in descending order)
+print('Number of candidates before NMS: ', len(decoded_candidates))
+boxes_nms = non_maximum_supression(decoded_candidates)
+print('Number of boxes after NMS: ', len(boxes_nms))
+
+# draw the boxes
+draw_bboxes(boxes_nms)
 
 
 
